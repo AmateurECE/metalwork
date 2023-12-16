01282c8092cc081f98d8574982bb461ee015e708
Ethan D. Twardy <ethan.twardy@gmail.com>
2023-12-16 09:50:23 -0600

    CMake: Add srec as a supported format to target_generate_artifact

--- test/cmake.sh
+++ test/cmake.sh
@@ -185,6 +185,40 @@ EOF
   test -f $test_directory/builddir/test.bin
 }
 
+test_target_generate_artifact_srec() {
+  local lib_directory=$1
+  local test_directory=$2
+
+  cat - <<EOF >$test_directory/toolchain.cmake
+  set(CMAKE_SYSTEM "Linux")
+  set(CMAKE_C_COMPILER $(which gcc))
+  set(CMAKE_CXX_COMPILER $(which g++))
+EOF
+
+  cat - <<'  EOF' >$test_directory/CMakeLists.txt
+  cmake_minimum_required(VERSION 3.25)
+  project(test)
+  include(${METALWORK_CMAKE_LIB_PATH}/metalwork.cmake)
+  add_executable(test test.c)
+  target_generate_artifact(test SREC)
+  EOF
+
+  cat - <<'  EOF' >$test_directory/test.c
+  int main() {
+    volatile int test = 0;
+    while (1) test++;
+  }
+  EOF
+
+  (cd $test_directory \
+    && cmake -B builddir -G Ninja \
+      -DMETALWORK_CMAKE_LIB_PATH=$lib_directory \
+      -DCMAKE_TOOLCHAIN_FILE=$test_directory/toolchain.cmake \
+    && ninja -C builddir)
+
+  test -f $test_directory/builddir/test.srec
+}
+
 CMAKE_LIB_DIRECTORY=$(realpath $1)
 TEST_DIRECTORY=$(realpath $2)
 
@@ -193,6 +227,7 @@ run_test test_target_set_link_script $CMAKE_LIB_DIRECTORY $TEST_DIRECTORY
 run_test test_target_generate_map_file $CMAKE_LIB_DIRECTORY $TEST_DIRECTORY
 run_test test_target_generate_artifact_hex $CMAKE_LIB_DIRECTORY $TEST_DIRECTORY
 run_test test_target_generate_artifact_bin $CMAKE_LIB_DIRECTORY $TEST_DIRECTORY
+run_test test_target_generate_artifact_srec $CMAKE_LIB_DIRECTORY $TEST_DIRECTORY
 
 summary
 
