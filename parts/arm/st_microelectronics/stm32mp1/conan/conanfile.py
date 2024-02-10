d18693f7a2621293bcb1e850827af8f54d290d66
Ethan D. Twardy <ethan.twardy@gmail.com>
2024-02-09 21:34:34 -0600

    stm32mp1/conan: Patch sources and update recipe
    The recipe should now allow compilation of executables that depend on
    the BSP.

--- parts/arm/st_microelectronics/stm32mp1/conan/conanfile.py
+++ parts/arm/st_microelectronics/stm32mp1/conan/conanfile.py
@@ -1,6 +1,7 @@
 from conan import ConanFile
 from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout
-from conan.tools.files import get
+from conan.tools.files import get, copy, apply_conandata_patches, export_conandata_patches
+import glob
 
 
 class stm32mp1Recipe(ConanFile):
@@ -26,26 +27,25 @@ class stm32mp1Recipe(ConanFile):
         'device': 'STM32MP157Dxx',
     }
 
-    # TODO: Put logic to generate CMakeLists.txt in custom Python generator
-    # https://docs.conan.io/2.0/reference/conanfile/methods/generate.html
-    exports_sources = "CMakeLists.txt"
-
     def layout(self):
         cmake_layout(self)
 
+    def export_sources(self):
+        # TODO: Put logic to generate CMakeLists.txt in custom Python generator
+        # https://docs.conan.io/2.0/reference/conanfile/methods/generate.html
+        copy(self, "CMakeLists.txt", self.recipe_folder, self.export_sources_folder)
+        export_conandata_patches(self)
+
     def source(self):
         # TODO: Put this in a conandata.yml
-        get(
-            self,
-            "https://github.com/STMicroelectronics/STM32CubeMP1/archive/refs/tags/1.6.0.tar.gz",
-            sha256="db5fa905a7ddd827e046fb2516b79c13f106f8a36ab401c45c68ba60e1a38cf3",
-            strip_root=True)
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def generate(self):
         tc = CMakeToolchain(self, generator="Ninja")
         tc.generate()
 
     def build(self):
+        apply_conandata_patches(self)
         cmake = CMake(self)
         cmake.configure(variables={'DEVICE': self.options.device})
         cmake.build()
@@ -55,9 +55,10 @@ class stm32mp1Recipe(ConanFile):
         cmake.install()
 
     def package_info(self):
+        device = str(self.options.device)
         self.cpp_info.components['hal'].libs = ['stm32mp1xx_hal_driver']
         self.cpp_info.components['hal'].includedirs = ['include/stm32mp1xx/hal']
-        flags = ['-DUSE_HAL_DRIVER', '-D' + str(self.options.device), '-DCORE_CM4']
+        flags = ['-DUSE_HAL_DRIVER', f'-D{device}', '-DCORE_CM4']
         # TODO: Can we remove the -include stdint.h?
         self.cpp_info.components['hal'].cflags = ['-include', 'stdint.h'] + flags
         self.cpp_info.components['hal'].cxxflags = ['-include', 'cstdint'] + flags
@@ -67,17 +68,11 @@ class stm32mp1Recipe(ConanFile):
         self.cpp_info.components['disco_bsp'].requires = ['hal']
 
         self.cpp_info.components['eval_bsp'].libs = ['stm32mp1xx_eval_bsp']
+        self.cpp_info.components['eval_bsp'].includedirs = ['include/stm32mp1xx/bsp']
+        self.cpp_info.components['eval_bsp'].requires = ['hal']
 
         # NOTE: Conan apparently changes the extension of the installed
         # object files from '.obj' to '.o'
-        # TODO: Just iterate over the objects in the libdir.
-        self.cpp_info.components['c_polyfill'].objects = [
-            'lib/syscalls.c.o',
-            'lib/sysmem.c.o',
-            'lib/startup_stm32mp157cxx_cm4.s.o',
-            'lib/system_stm32mp1xx.c.o',
-        ]
+        self.cpp_info.components['c_polyfill'].objects = glob.glob(self.package_folder + '/lib/*.o')
         # TODO: Make linker script an option
-        self.cpp_info.components['c_polyfill'].exelinkflags = [
-            '-T', 'STM32MP157CAAX_RAM.ld'
-        ]
+        self.cpp_info.components['c_polyfill'].exelinkflags = ['-T', 'STM32MP157CAAX_RAM.ld']
